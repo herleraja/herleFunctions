3839c8f6d75f93a183ddfe9f4dabb712447b126f
amritharajherle <amritha.herle@ibm.com>
2020-04-30 17:40:59 +0530

    changing variable names.

--- custom_functions/basic_if_then_else_functions.py
+++ custom_functions/basic_if_then_else_functions.py
@@ -31,20 +31,20 @@ class BasicIfThenElse(BaseTransformer):
     false value: Online
     """
 
-    def __init__(self, input_items, conditional_expression, true_value, false_value, output_item=None):
+    def __init__(self, input_items, if_conditional_expression, then_true_value, else_false_value, output_item=None):
         super().__init__()
 
         self.input_items = input_items
-        self.conditional_expression = self.parse_expression(conditional_expression)
-        self.true_value = true_value
-        self.false_value = false_value
+        self.if_conditional_expression = self.parse_expression(if_conditional_expression)
+        self.then_true_value = then_true_value
+        self.else_false_value = else_false_value
         if output_item is None:
             self.output_item = 'output_item'
         else:
             self.output_item = output_item
 
     def execute(self, df):
-        df[self.output_item] = np.where(eval(self.conditional_expression), self.true_value, self.false_value)
+        df[self.output_item] = np.where(eval(self.if_conditional_expression), self.then_true_value, self.else_false_value)
         return df
 
     @classmethod
@@ -52,10 +52,10 @@ class BasicIfThenElse(BaseTransformer):
         # define arguments that behave as function inputs
         inputs = []
         inputs.append(UIMultiItem('input_items', description="data items that are used in conditional_expression"))
-        inputs.append(UIExpression(name='conditional_expression',
+        inputs.append(UIExpression(name='if_conditional_expression',
                                    description="expression that returns a specified value, eg. if df['temp']>50 then Offline else Online"))
-        inputs.append(UISingle(name='true_value', description="value when true, eg. Offline"))
-        inputs.append(UISingle(name='false_value', description='value when false, eg. Online '))
+        inputs.append(UISingle(name='then_true_value', description="value when true, eg. Offline"))
+        inputs.append(UISingle(name='else_false_value', description='value when false, eg. Online '))
         # define arguments that behave as function outputs
         outputs = []
         outputs.append(UIFunctionOutSingle(name='output_item', description='function output'))
