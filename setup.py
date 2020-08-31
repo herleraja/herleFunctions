004b818bd68dd48708ffa590b630b465bd6e075c
amritharajherle <amritha.herle@ibm.com>
2020-08-31 09:23:24 +0200

    Removing kafka reference since its already added by iotfunctions.

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@
 from setuptools import setup, find_packages
 
 setup(name='custom_functions', version='0.0.1', packages=find_packages(),
-      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@dev'],
-      extras_require={'kafka': ['confluent-kafka==0.11.5']})
+      install_requires=['iotfunctions@git+https://github.com/ibm-watson-iot/functions.git@dev'])
